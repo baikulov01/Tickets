59d88ab3917862bcf03fdfe3789b3a6d50dba9da
xricherenkova <xricherenok01@gmail.com>
2022-11-25 19:40:59 +0400

    added elements for registration

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,19 @@ use Illuminate\Support\Facades\Route;
 
 
 
+<<<<<<< Updated upstream
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/tickets', 'TicketController@index')->name('tickets.index');
+=======
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+>>>>>>> Stashed changes
